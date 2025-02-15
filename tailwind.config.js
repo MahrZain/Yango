196da2efe4aaa3470eb99fc7f10eb06e6e2dbfa5
Mehar Zain <zainzainpubg1122@gmail.com>
2025-02-15 19:07:34 +0500

    refactor: replace style.css with input.css for Tailwind CSS integration

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*"], // This scans all files in the project
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
