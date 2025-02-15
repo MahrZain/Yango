309310db11996c528d041126e847a0ebd2d2220d
Mehar Zain <zainzainpubg1122@gmail.com>
2025-02-15 19:14:00 +0500

    refactor: remove unused Tailwind CSS files and dependencies

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["*"], // This scans all files in the project
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
-  
